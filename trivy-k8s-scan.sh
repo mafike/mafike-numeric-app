dcb08cdded9470295297056a4aeb5a7a6631a6af
mafike <mafgenes1@icloud.com>
2024-12-20 16:46:14 -0500

    fixed trivy

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -2,22 +2,34 @@
 
 #!/bin/bash
 
+echo "Image Name: $imageName" # Getting Image name from the environment variable
 
-echo $imageName #getting Image name from env variable
+# Set the cache directory
+TRIVY_CACHE_DIR="$WORKSPACE/trivy-cache"
+mkdir -p "$TRIVY_CACHE_DIR"
+chown -R $(id -u jenkins):$(id -g jenkins) "$TRIVY_CACHE_DIR"
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity CRITICAL --light $imageName
+# Run Trivy scans
+docker run --rm \
+  -v "$WORKSPACE:/root/.cache/" \
+  -e "TRIVY_CACHE_DIR=/root/.cache/trivy-cache" \
+  aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light "$imageName"
 
-    # Trivy scan result processing
-    exit_code=$?
-    echo "Exit Code : $exit_code"
+docker run --rm \
+  -v "$WORKSPACE:/root/.cache/" \
+  -e "TRIVY_CACHE_DIR=/root/.cache/trivy-cache" \
+  aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light "$imageName"
 
-    # Check scan results
-    if [[ ${exit_code} == 1 ]]; then
-        echo "Image scanning failed. Vulnerabilities found"
-        exit 1;
-    else
-        echo "Image scanning passed. No vulnerabilities found"
-    fi;
+# Trivy scan result processing
+exit_code=$?
+echo "Exit Code: $exit_code"
 
-############### trivy-k8s-scan.sh ############### 
+# Check scan results
+if [[ "${exit_code}" == 1 ]]; then
+    echo "Image scanning failed. Vulnerabilities found."
+    exit 1
+else
+    echo "Image scanning passed. No vulnerabilities found."
+fi
+
+############### trivy-k8s-scan.sh ###############
