c8c04f2fa28a591018be210f25aa077c656dbf46
mafike <mafgenes1@icloud.com>
2024-12-22 13:41:06 -0500

    test

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -5,13 +5,13 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Ensure required variables are set
-if [[ -z "${imageName}" || -z "${deploymentName}" ]]; then
-  echo "Error: Required environment variables (imageName, deploymentName) are not set."
+if [[ -z "${SimageName}" || -z "${deploymentName}" ]]; then
+  echo "Error: Required environment variables (SimageName, deploymentName) are not set."
   exit 1
 fi
 
 # Replace the placeholder in the deployment YAML file with the actual image name
-sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+sed -i "s#replace#${SimageName}#g" k8s_deployment_service.yaml
 
 # Check if the deployment exists
 if ! kubectl -n "${namespace}" get deployment "${deploymentName}" > /dev/null 2>&1; then
@@ -20,7 +20,7 @@ if ! kubectl -n "${namespace}" get deployment "${deploymentName}" > /dev/null 2>
   kubectl -n "${namespace}" apply -f k8s_deployment_service.yaml
 else
   echo "Deployment ${deploymentName} exists. Updating the container image..."
-  kubectl -n "${namespace}" set image deployment/${deploymentName} ${containerName}=${imageName} --record=true
+  kubectl -n "${namespace}" set image deployment/${deploymentName} ${containerName}=${SimageName} --record=true
 fi
 
 echo "Deployment command executed successfully."
